1bd5ddff971e546d74639be17188da2e8b5ef04e
Alan <alan.attard@gig.com>
2019-06-18 07:15:57 +0200

    Added host configuration validation

--- SimpleCQRS.Host/HostFactory.cs
+++ SimpleCQRS.Host/HostFactory.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using SimpleCQRS.Contracts.Exceptions;
 using SimpleCQRS.Host.Configuration;
 
 namespace SimpleCQRS.Host
@@ -10,6 +12,24 @@ namespace SimpleCQRS.Host
             var config = new HostConfiguration();
             configure(config);
 
+            if (string.IsNullOrEmpty(config.ServiceName))
+            {
+                throw new SimpleCQRSException($"{nameof(config.ServiceName)} must be provided.");
+            }
+
+            if (!config.Operations.Any())
+            {
+                throw new SimpleCQRSException("At least 1 operation must be provided.");
+            }
+
+            config.Operations.ForEach(o =>
+            {
+                if (string.IsNullOrEmpty(o.OperationName))
+                {
+                    throw new SimpleCQRSException($"{nameof(o.OperationName)} must be provided.");
+                }
+            });
+            
             return new CQRSHost(config);
         }
     }
